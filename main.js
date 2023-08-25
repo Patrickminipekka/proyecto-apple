2e26ec0ec182053c0249e9b5bd9c7b589a46a559
PATRICK :D <119462581+Patrickminipekka@users.noreply.github.com>
2023-08-25 09:29:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+screen_width = 0;
+screen_height = 0;
+
+var apple='';
+
+var speech_data='';
+
+ var to_number=0;
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function preload(){
+  apple=loadImage("apple.png");
+}
+
+function start()
+{
+  document.getElementById("status").innerHTML = "El sistema está escuchando. Por favor, habla.";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "La voz se reconoció como: " + content; 
+to_number=Number(content);
+if(Number.isInteger(to_number)){
+  
+  document.getElementById("status").innerHTML = "se empezo a dibujar una manzana";
+  draw_apple="set";
+}
+else{
+  document.getElementById("status").innerHTML = "no se reconocio el número";
+}
+}
+
+function setup() {
+ screen_width=window.innerWidth;
+ screen_height=window.innerHeight;
+ canvas=createCanvas(screen_width,screen_height-150);
+ canvas.position(0,150);
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    for(var i=1;)
+    document.getElementById("status").innerHTML = to_number + " manzanas dibujadas";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+
+for (var i = 1; i <= to_number; i++)
+{
+  x = Math.floor(Math.random() *700);
+  y = Math.floor(Math.random() *400);
+  image(apple,x ,y, 50, 50);
+}
